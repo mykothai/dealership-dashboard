0ebe62179710760dde936f5268b2d62050d2e7a8
myko <mikethaivl@gmail.com>
2025-01-25 14:53:48 -0800

    add api calls for vehicles

--- src/api/VehicleApi.tsx
+++ src/api/VehicleApi.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios'
 import instance from './axiosConfig'
+import { Vehicle } from '@components/Vehicles/VehicleDashboard'
 
 export async function getAllVehicles(): Promise<AxiosResponse> {
   try {
@@ -9,6 +10,47 @@ export async function getAllVehicles(): Promise<AxiosResponse> {
       throw err
     } else {
       throw new Error(`There was an error retrieving vehicles. ${err}`)
+    }
+  }
+}
+
+export async function createVehicle(
+  payload: Partial<Vehicle>
+): Promise<AxiosResponse> {
+  try {
+    return await instance.post('/inventory/vehicle', payload)
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      throw err
+    } else {
+      throw new Error(`There was an error adding a vehicle. ${err}`)
+    }
+  }
+}
+
+export async function updateVehicle(
+  id: number,
+  payload: Partial<Vehicle>
+): Promise<AxiosResponse> {
+  try {
+    return await instance.put(`/inventory/vehicle/${id}`, payload)
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      throw err
+    } else {
+      throw new Error(`There was an error updating the vehicle. ${err}`)
+    }
+  }
+}
+
+export async function deleteVehicle(id: number): Promise<AxiosResponse> {
+  try {
+    return await instance.delete(`/inventory/vehicle/${id}`)
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      throw err
+    } else {
+      throw new Error(`There was an error deleting the vehicle. ${err}`)
     }
   }
 }
