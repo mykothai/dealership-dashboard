bca30032507a4a1ca0d0e372d739a0eb514e6173
myko <mikethaivl@gmail.com>
2025-01-27 14:17:32 -0800

    use navigate on 404

--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,15 +1,16 @@
+import { useNavigate } from 'react-router-dom'
 import './NotFoundPage.css'
 import { TbError404 } from 'react-icons/tb'
 
 export default function NotFoundPage() {
+  const navigate = useNavigate()
+
   return (
     <div className="wrapper">
       <TbError404 className="not-found" />
       <div className="text-center">
         <p>Oops! The page you are looking for could not be found.</p>
-        <button onClick={() => (window.location.href = '/')}>
-          Go Back to the Login Screen
-        </button>
+        <button onClick={() => navigate('/')}>Go Back</button>
       </div>
     </div>
   )
